f7c6a58b2ec0b103b250e5facc625c6c233c006d
Luis Redda <luis.redda@gmail.com>
2023-04-07 12:32:32 -0300

    Delete function.py

--- function.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def hello_world(request):
-    canary = os.environ.get('CANARY')
-    if canary == 'true':
-        message = 'Hello canary!'
-    else:
-        message = 'Hello stable!'
-    return message
