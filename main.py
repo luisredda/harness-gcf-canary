d81fcc35ca5f743ca27298ef3a25cd189be5404c
Luis Redda <luis.redda@gmail.com>
2023-04-10 20:58:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
 
 def hello_world(request):
+    
+     headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    }
+    
+    if request.method == 'OPTIONS':
+        # Handle CORS preflight request
+        return ('', 204, headers)
+    
     canary = os.environ.get('CANARY')
     if canary == 'true':
         message = 'Hello canary!'
