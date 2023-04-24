927260584ebbdb2225b612231ea2d8d228ab8409
Luis Redda <luis.redda@gmail.com>
2023-04-24 17:44:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
+import time  
+from featureflags.evaluations.auth_target import Target  
+from featureflags.client import CfClient  
+from featureflags.util import log  
+from featureflags.config import with_base_url  
+from featureflags.config import with_events_url  
 
 def hello_world(request):
     
@@ -14,9 +20,15 @@ def hello_world(request):
 
     revision = os.environ.get('K_REVISION')
     message = 'Hello Harness!'
+    ff_status = evaluate_ff()
+    
+    if ff_status == 'True':
+        time.sleep(1000)
+   
     response = {
         'revision': revision,
-        'message': message
+        'message': message,
+        'ff_enabled': ff_status
     }
 
     # Set CORS headers for the main request
@@ -25,3 +37,14 @@ def hello_world(request):
     }
 
     return (response, 200, headers)
+
+
+def evaluate_ff():
+    api_key = os.environ.get('GCF_FF_KEY')  
+    client = CfClient(api_key,  
+                      with_base_url("https://config.ff.harness.io/api/1.0"),  
+                      with_events_url("https://events.ff.harness.io/api/1.0"))  
+  
+    target = Target(identifier='GCF_Api_Guest') 
+    result = client.bool_variation('GCF_API_Feature', target, False)  
+    return result
